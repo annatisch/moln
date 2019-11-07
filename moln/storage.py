03721a0e07c2e3c2d3e1314d968844f0d7f7f26b
Johan Stenberg <johan.stenberg@microsoft.com>
2019-11-06 17:35:17 -0800

    Black + silly mistakes fixed.

--- moln/storage.py
+++ moln/storage.py
@@ -24,20 +24,19 @@ import azure.core.exceptions
 import azure.identity
 import azure.storage.blob
 
-__all__ = [
-    "AzurePath",
-    "attach"
-]
+__all__ = ["AzurePath", "attach"]
+
 
 class AzurePath(abc.ABC):
     """pathlib.Path-like object for interacting with Azure Storage Blobs.
     """
 
     @abc.abstractmethod
-    def __truediv__(self, other): ...
+    def __truediv__(self, other):
+        ...
 
     @abc.abstractmethod
-    def mkdir(self, *, exists_ok:bool=False, **kwargs):
+    def mkdir(self, *, exists_ok: bool = False, **kwargs):
         """Create a new directory (container)
 
         Only supported for container-level paths. 
@@ -45,9 +44,8 @@ class AzurePath(abc.ABC):
         :param exists_ok: Don't report an error if directory (container) already exists.
         """
 
-    
     @abc.abstractmethod
-    def open(self, *, mode='r', **kwargs):
+    def open(self, *, mode="r", **kwargs):
         """Open the given file (blob)
 
         Only supported for blob-level paths.
@@ -59,29 +57,33 @@ class AzurePath(abc.ABC):
     def is_file(self):
         return False
 
+
 class AccountPath(AzurePath):
-    
     def __init__(self, *, account_url=None, connection_string=None, **kwargs):
         if not account_url and not connection_string:
-            raise ValueError('Must specify either a connection string or an account_url')
+            raise ValueError(
+                "Must specify either a connection string or an account_url"
+            )
 
         if connection_string:
-            self.client = azure.storage.blob.BlobServiceClient.from_connection_string(connection_string, **kwargs)
+            self.client = azure.storage.blob.BlobServiceClient.from_connection_string(
+                connection_string, **kwargs
+            )
         else:
-            kwargs.setdefault('credential', azure.identity.DefaultAzureCredential())
+            kwargs.setdefault("credential", azure.identity.DefaultAzureCredential())
             self.client = azure.storage.blob.BlobServiceClient(account_url, **kwargs)
 
     def __truediv__(self, other):
         return ContainerPath(other, self.client.get_container_client(other))
 
-    def open(self, *, mode='r', **kwargs):
+    def open(self, *, mode="r", **kwargs):
         raise TypeError(f'Unable to open an account path for "{mode}""')
 
     def mkdir(self, exists_ok=False, **kwargs):
         raise TypeError("Unable to mkdir an account.")
 
+
 class ContainerPath(AzurePath):
-    
     def __init__(self, container_name, client):
         self.name = container_name
         self.client = client
@@ -92,34 +94,39 @@ class ContainerPath(AzurePath):
         except azure.core.exceptions.ResourceExistsError as e:
             if not exists_ok:
                 raise
-    
-    def open(self, *, mode='r', **kwargs):
+
+    def open(self, *, mode="r", **kwargs):
         raise TypeError(f'Unable to open a container path for "{mode}""')
 
     def exists(self):
         try:
             self.client.get_container_properties()
             return True
-        except azure.core.ResoureNotFoundError:
+        except azure.core.exceptions.ResourceNotFoundError:
             return False
 
     def glob(self, pattern, **kwargs):
-        if pattern.startswith('**/'):
-            recursive = True
+        # ISSUE - should we manufacure "directories" when globbing blobs?
+        if pattern.startswith("**/"):
+            # recursive = True
             pattern = pattern[3:]
-        else:
-            recursive = False
+        #else:
+        #    recursive = False
 
-        if pattern[0] == '.':
-            leading_dot = True
+        if pattern[0] == ".":
+            # leading_dot = True
             pattern = pattern[1:]
-        else:
-            leading_dot = False
+        # else:
+        #    leading_dot = False
 
         for blob in self.client.list_blobs():
             if fnmatch.fnmatch(blob.name, pattern):
-                yield BlobPath(blob_name=blob.name, container_name=self.name, client=self.client.get_blob_client(blob))
-                
+                yield BlobPath(
+                    blob_name=blob.name,
+                    container_name=self.name,
+                    client=self.client.get_blob_client(blob),
+                )
+
     def is_dir(self):
         return self.exists()
 
@@ -127,11 +134,11 @@ class ContainerPath(AzurePath):
         return BlobPath(
             container_name=self.name,
             blob_name=other,
-            client=self.client.get_blob_client(other)
+            client=self.client.get_blob_client(other),
         )
 
+
 class BlobPath(AzurePath):
-    
     def __init__(self, container_name, blob_name, client):
         self.container_name = container_name
         self.name = blob_name
@@ -139,7 +146,9 @@ class BlobPath(AzurePath):
 
     def __truediv__(self, other):
         # TODO - what should the correct behavior be here? Append a '/<other>' to the name or raise? Or give me a snapshot?
-        raise NotImplementedError('Undone - navigate below blogs is currently not implemented')
+        raise NotImplementedError(
+            "Undone - navigate below blogs is currently not implemented"
+        )
 
     def exists(self):
         try:
@@ -151,20 +160,20 @@ class BlobPath(AzurePath):
     def is_file(self):
         return self.exists()
 
-    def open(self, *, mode='r', **kwargs):
-        if 'r' in mode:
+    def open(self, *, mode="r", **kwargs):
+        if "r" in mode:
             return DownloadStream(self.client)
-        elif 'w' in mode:
+        elif "w" in mode:
             return UploadStream(self.client, **kwargs)
 
     def mkdir(self, exists_ok=False, **kwargs):
         raise TypeError("Unable to mkdir a blob.")
 
     def __repr__(self):
-        return f'/{self.container_name}/{self.name}'
+        return f"/{self.container_name}/{self.name}"
+
 
 class DownloadStream(io.RawIOBase):
-
     def __init__(self, client: azure.storage.blob.BlobClient):
         self.client = client
         self.position = 0
@@ -181,32 +190,31 @@ class DownloadStream(io.RawIOBase):
         data_to_read = len(b)
         done = False
         while not done:
-            read_data = read(data_to_read)
+            read_data = self.read(data_to_read)
             data_read = len(read_data)
 
             if data_read:
-                b[bufoffset:bufoffset + data_read] = read_data
+                b[bufoffset : bufoffset + data_read] = read_data
                 bufoffset += data_read
 
             done = data_read == 0 or bufoffset == len(b)
-        
-        return bufoffset
 
+        return bufoffset
 
     def readall(self):
         blob_properties = self.client.get_blob_properties()
-        data_to_read = blob_propertes.size
+        data_to_read = blob_properties.size
         buf = bytearray(data_to_read)
-        readinto(buf)
+        self.readinto(buf)
         return buf
-        
 
     def write(self):
-        raise NotImplementedError('Huh - someone tried to write to a download stream. Never cross the streams!')
+        raise NotImplementedError(
+            "Huh - someone tried to write to a download stream. Never cross the streams!"
+        )
 
 
 class UploadStream(io.BytesIO):
-
     def __init__(self, client, **kwargs):
         self.client = client
         self.kwargs = kwargs
@@ -222,11 +230,16 @@ class UploadStream(io.BytesIO):
         self.seek(0)
         self.client.upload_blob(self, **self.kwargs)
         super().close()
-    
-def attach(*, account_url: str=None, connection_string: str=None, **kwargs) -> AzurePath:
+
+
+def attach(
+    *, account_url: str = None, connection_string: str = None, **kwargs
+) -> AzurePath:
     """Attach to an existing azure storage account.
 
     :param account_url: Url of storage account to attach to. Required unless `connection_string` is provided.
     :param connection_string: Connection string of storage account to attach to. Required unless `account_url` is provided.
     """
-    return AccountPath(account_url=account_url, connection_string=connection_string, **kwargs)
+    return AccountPath(
+        account_url=account_url, connection_string=connection_string, **kwargs
+    )
